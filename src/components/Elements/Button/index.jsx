3c77c18b9ed8b219596e2e33e37cd4720d4901f5
Pratama89 <pratama.adiciptawan@gmail.com>
2023-09-21 16:43:54 +0700

    menambahkan tokped dan shopee

--- src/components/Elements/Button/index.jsx
+++ src/components/Elements/Button/index.jsx
@@ -1,18 +1,16 @@
 /* eslint-disable react/prop-types */
 const Button = (props) => {
     const {children, 
-      ClassName, 
-       
-      type = "submit",
+      ClassName      
     } = props;
     
     return (
-      <button 
-        className={`h-16 px-6 border rounded-full ${ClassName} text-white flex mx-auto  items-center justify-between hover:bg-white hover:text-blue-500 `} type={type}
+      <div 
+        className={`h-16 px-6 border rounded-full ${ClassName} text-white flex mx-auto  items-center justify-between hover:bg-white hover:text-blue-500 `} 
         
       >        
         {children}
-      </button>
+      </div>
       
     );
   };
--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -22,13 +22,32 @@ const FormLogin = () => {
             Facebook
           </Button>
           </a>
-          <a href="https://www.facebook.com/pharmadental.pharmadental">
+          <a href="https://www.tokopedia.com/pharmadent">
           <Button ClassName="bg-transparent w-full font-semibold text-lg my-5" type="submit"
           >
             <div className="rounded-full h-12 w-12 flex items-center justify-center -ml-4 " >              
-                <img src="/public/facebook.png" alt="" />
+                <img src="/public/Tokped.png" alt="" />
             </div>
-            Facebook
+            Tokopedia
+          </Button>
+          </a>
+          
+          <a href="https://shopee.co.id/shop/135955996">
+          <Button ClassName="bg-transparent w-full font-semibold text-lg my-5" type="submit"
+          >
+            <div className="rounded-full h-12 w-12 flex items-center justify-center -ml-4 " >              
+                <img src="/public/Shopee.png" alt="" />
+            </div>
+            Tokopedia
+          </Button>
+          </a>
+          <a href="wa.me/6282191629191">
+          <Button ClassName="bg-transparent w-full font-semibold text-lg my-5" type="submit"
+          >
+            <div className="rounded-full h-12 w-12 flex items-center justify-center -ml-4 " >              
+                <img src="/public/WA.png" alt="" />
+            </div>
+            Tokopedia
           </Button>
           </a>
         </form>
