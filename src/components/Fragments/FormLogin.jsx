6bcda438f75954bbb48a1967a5df815060a58d2d
Pratama89 <pratama.adiciptawan@gmail.com>
2023-09-23 10:08:45 +0700

    menambahkan tokped dan shopee repair

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -27,7 +27,7 @@ const FormLogin = () => {
           <Button ClassName="bg-transparent w-full font-semibold text-lg my-5" type="submit"
           >
             <div className="rounded-full h-12 w-12 flex items-center justify-center -ml-4 " >              
-                <img src="/public/Tokped_1.svg" alt="" />
+                <img src="/public/Tokped.svg" alt="" />
             </div>
             Tokopedia
           </Button>
@@ -37,7 +37,7 @@ const FormLogin = () => {
           <Button ClassName="bg-transparent w-full font-semibold text-lg my-5" type="submit"
           >
             <div className="rounded-full h-12 w-12 flex items-center justify-center -ml-4 " >              
-                <img src="/public/Shopee_1.svg" alt="" />
+                <img src="/public/Shopee.svg" alt="" />
             </div>
             Shopee
           </Button>
@@ -46,7 +46,7 @@ const FormLogin = () => {
           <Button ClassName="bg-transparent w-full font-semibold text-lg my-5" type="submit"
           >
             <div className="rounded-full h-12 w-12 flex items-center justify-center -ml-4 " >              
-                <img src="/public/Tiktok_1.svg" alt="" />
+                <img src="/public/Tiktok.svg" alt="" />
             </div>
             Tiktok Shop
           </Button>
@@ -55,7 +55,7 @@ const FormLogin = () => {
           <Button ClassName="bg-transparent w-full font-semibold text-lg my-5" type="submit"
           >
             <div className="rounded-full h-12 w-12 flex items-center justify-center -ml-4 " >              
-                <img src="/public/WA_1.svg" alt="" />
+                <img src="/public/WA.svg" alt="" />
             </div>
             WhatsApp
           </Button>
--- src/components/Layouts/AuthLayout.jsx
+++ src/components/Layouts/AuthLayout.jsx
@@ -9,7 +9,7 @@ const AuthLayout = (props) => {
       <div className='flex justify-center min-h-screen bg-blue-500 items-center'>
           <div className='w-full max-w-2xl  p-5 rounded-lg  -mt-36'>
             <div className="">
-            <img src="/public/Pharmadent Telp.jpg" alt="" className="h-24 w-24 mx-auto rounded-full mb-3 " />
+            <img src="/public/Pharmadent Telp.svg" alt="" className="h-24 w-24 mx-auto rounded-full mb-3 " />
             </div>
           <h1 className='text-3xl font-bold mb-10 text-center text-white '>{title}</h1>        
 
